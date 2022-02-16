f6e108277e8632a24f1c5c8a3b5900c654cb5553
Leonardo Gasperin <leonardo.a.gasperin@gmail.com>
2022-02-16 16:16:15 -0300

    Functional

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import PySimpleGUI as sg
 from fpdf import FPDF
 from datetime import datetime
 from babel.dates import format_datetime
@@ -15,17 +16,35 @@ else:
 img = cv2.imread('breakingnews.png')
 
 text = pytesseract.image_to_string('temp.png')
-print(text)
+valores = text.split('\n')
 
-mes_inicial = 1
-ano_inicial = 2021
+hoje = format_datetime(datetime.today(), "'Data:' d 'de' MMMM 'de' Y", locale='pt_BR')
 
-inicio = datetime(ano_inicial, mes_inicial, 1)
+layout = [[sg.Text('Digite o mês inicial em número:')],
+          [sg.InputText()],
+          [sg.Text('Digite o ano inicial:')],
+          [sg.InputText()],
+          [sg.Checkbox('Inverso', key='inverso')],
+          [sg.Submit(), sg.Cancel()]]
+
+window = sg.Window('Faturamento Automático', layout)
+
+event, values = window.read()
+window.close()
+
+if values['inverso']:
+    valores.reverse()
+    del valores[0]
+
+mes_inicial = int(values[0])
+ano_inicial = int(values[1])
+
+inicio = datetime(ano_inicial, mes_inicial, 10)
 datas = []
 
 for dt in rrule(freq=MONTHLY, count=12, dtstart=inicio):
     datas.append(format_datetime(dt,'MMMM/Y' ,locale='pt_BR').capitalize())
-
+    
 print(datas)
 
 nome = "Cristiane Homma"
@@ -37,18 +56,28 @@ texto = (
     "período de " + datas[0] + " a " + datas[-1]
 )
 
-print(texto)
-
 pdf = FPDF()
 pdf.add_page()
-pdf.set_font('Arial', size = 16)
+pdf.set_font('Arial', 'B', size = 16)
 pdf.cell(200,
          10, 
          txt = "DECLARAÇÃO DE FATURAMENTO", 
          ln = 1,
          align = 'C')
-
-pdf.set_font('Arial', size = 14)
+pdf.ln(7)
+pdf.set_font('Arial', size = 12)
 pdf.write(8, texto)
-
-pdf.output("Declaração de Faturamento.pdf") 
+pdf.ln(15)
+col_width = pdf.epw/2
+line_height = pdf.font_size*2
+for i in range(len(datas)):
+    pdf.multi_cell(col_width, line_height, datas[i], border=1, ln=3)
+    pdf.multi_cell(col_width, line_height, valores[i], border=1, ln=3)
+    pdf.ln(line_height)
+pdf.ln(15)
+pdf.cell(200,
+         10,
+         txt = hoje + "\t",
+         ln = 1,
+         align = 'R') 
+pdf.output("Declaração de Faturamento.pdf")
